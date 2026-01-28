8a790e3b4d40f41ef5048f491bfea566b6039aff
ankit <ankityadav8299blp@gmail.com>
2026-01-28 21:46:58 +0530

    fix cors and preflight for vercel

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,35 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-dotenv.config();
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
 
+dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+
+// ✅ FIXED CORS
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+// ✅ FIXED PREFLIGHT
+app.options("*", cors());
+
 app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
 app.use("/", require("./routes/userRoute"));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
