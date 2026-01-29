18818eaecb69b5f81073a5353de1ac02acbf72db
ankit <ankityadav8299blp@gmail.com>
2026-01-29 14:44:58 +0530

    corrected cors

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: ["https://easytrips-sigma.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-  }),
-);
-
-// ❌ REMOVE app.options("*", cors());
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(cookieParser());
 
