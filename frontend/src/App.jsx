fc80d7f2e3e9bc693d62440024c52bdc412d2446
ankit <ankityadav8299blp@gmail.com>
2026-01-30 11:08:12 +0530

    semicolon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ import { AuthProvider } from './context/AuthContext'
 import AddDestination from "./pages/AddDestination"
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./App.css"
+import "./App.css";
 
 function AppRoutes() {
   return (
